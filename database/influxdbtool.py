e1511b248b5cc8be228050ca618ca25cbced60dd
ZYFZYF <973446154@qq.com>
2022-02-27 22:44:32 +0800

    时序数据元信息自动存储

--- database/influxdbtool.py
+++ database/influxdbtool.py
@@ -20,8 +20,29 @@ class InfluxdbTool(DatabaseTool):
     dataframe_client = DataFrameClient(host, port, username, password, dbname)
 
     @classmethod
+    def load_time_series_meta_data(cls, name) -> dict:
+        res = cls.client.query(f'select * from time_series_meta where \"name\"=\'{name}\'')
+        tmp = {}
+        for w in res.get_points('time_series_meta'):
+            for k, v in w.items():
+                if k != 'time' and k != 'name':
+                    tmp[k] = v
+        return tmp
+
+    @classmethod
+    def save_time_series_meta_data(cls, time_series, name):
+        for k, v in time_series.__dict__.items():
+            if isinstance(v, int) or isinstance(v, float) or isinstance(v, str):
+                cls.client.write_points(points=[{'measurement': 'time_series_meta',
+                                                 'tags': {'name': name},
+                                                 'time': 0,
+                                                 'fields': {k: v}}])
+
+    @classmethod
     def load_time_series(cls, name) -> TimeSeries:
-        return TimeSeries(cls.dataframe_client.query(f'select * from {name}')[name])
+        res = TimeSeries(cls.dataframe_client.query(f'select * from {name}')[name])
+        res.__dict__.update(cls.load_time_series_meta_data(name))
+        return res
 
     @classmethod
     def load_dataset(cls, name):
@@ -33,6 +54,7 @@ class InfluxdbTool(DatabaseTool):
     @classmethod
     def save_time_series(cls, name, time_series: TimeSeries):
         cls.dataframe_client.write_points(time_series, name)
+        cls.save_time_series_meta_data(time_series, name)
 
     @classmethod
     def save_dataset(cls, dataset: Dataset):
@@ -51,13 +73,8 @@ class InfluxdbTool(DatabaseTool):
 if __name__ == '__main__':
     client = InfluxdbTool()
     df = TimeSeries(data={'a': [1, 2, 3], 'b': [4, 5, 6], 'label': [0, 0, 0]})
-    print(df)
-    print(df.index)
-    # df.index = pd.DatetimeIndex(df.index)
-    print(df)
-    client.dataframe_client.write_points(df, measurement='demo')
-    print(client.dataframe_client.query("select * from demo"))
-    x = client.dataframe_client.query("select * from demo")
-    y = x['demo']
-    print(y, y.index)
-    client.dataframe_client.drop_measurement('demo')
+    df.test = 'TAT'
+    print(df.__dict__)
+    client.save_time_series('demo', df)
+    tf = client.load_time_series('demo')
+    print(tf, tf.__dict__)
--- dataset/time_series.py
+++ dataset/time_series.py
@@ -5,12 +5,14 @@ from abc import ABCMeta
 import pandas
 import pandas as pd
 
+from utils.config import CONFIG
+
 
 class TimeSeries(pandas.DataFrame, metaclass=ABCMeta):
     def __init__(self, *args, **kwargs):
         super(TimeSeries, self).__init__(*args, **kwargs)
+        # 判断是否有时间索引
         if not isinstance(self.index, pandas.DatetimeIndex):
-            timestamp = None
             for ts_column in ['ts', 'time', 'timestamp']:
                 if ts_column in self.columns and self[ts_column].dtype == float:
                     ratio = 10 ** (max(0, 19 - len(str(int(self[ts_column][0])))))
@@ -23,25 +25,36 @@ class TimeSeries(pandas.DataFrame, metaclass=ABCMeta):
                 # 默认从0开始，一分钟一个点
                 timestamp = [i * 60 * 1e9 for i in range(len(self))]
             self.index = pd.to_datetime(timestamp)
+        # 判断是否传入了
         if 'label' not in self.columns:
             raise ValueError("should have label(0 for anomaly 1 for normal) column")
 
-    def split(self, timestamp):
-        return TimeSeries(self[self.timestamp < timestamp]), TimeSeries(
-            self[self.timestamp >= timestamp])
+    def split(self):
+        if hasattr(self, 'train_data_length'):
+            print('has!!!')
+            split_pos = self.train_data_length
+        else:
+            print('not has!!!')
+            split_pos = int(len(self) * CONFIG.get('dataset').get('split_ratio'))
+
+        return TimeSeries(self.iloc[:split_pos]), TimeSeries(self.iloc[split_pos:])
 
     @classmethod
     def union(cls, train: pandas.DataFrame, test: pandas.DataFrame):
-        return cls(pandas.concat([train, test]))
+        res = cls(pandas.concat([train, test]))
+        res.train_data_length = len(train)
+        return res
 
 
 if __name__ == '__main__':
     df = TimeSeries(data={'timestamp': [1, 2, 3, 4, 5], 'value': [100, 200, 300, 400, 500], 'label': [0, 0, 1, 1, 1]})
     print(df)
-    train, test = df.split(3)
+    train, test = df.split()
     print(train)
     print(test)
-    print(TimeSeries.union(train, test))
+    wf = TimeSeries.union(train, test)
+    print(wf)
+    print(wf.split())
     tf = pandas.DataFrame(df)
     print(tf)
     df = TimeSeries(data={'value': [3, 4, 5, 6, 200, 300, 400, 500], 'label': [0, 0, 1, 0, 1, 0, 1, 1]})
